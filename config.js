d510f9e47490770fc6eda10b1290546fb99ca645
Rirtar <86695952+Rirtar@users.noreply.github.com>
2021-07-04 00:09:22 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Bae à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnIntro: 'ngại quá đi à :v',
+    title: 'EM CÓ MUỐN LÀM NGƯỜI YÊU ANH KHÔNG 😙',
+    desc: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên :333 ',
+    btnYes: 'LOVE U 3000 :3 ',
+    btnNo: 'Không đâu mơ đi :)))',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    btnReply: 'Ahihi <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <3333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
